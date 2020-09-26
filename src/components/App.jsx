0d86e0a88850507279e1d5e7ef00db9be03a89eb
Oaes Kuruni <oaeskuruni27@gmail.com>
2020-09-26 15:28:04 +0600

    profile section added

---
+++ src/components/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Profile from "./profile";
+
+class App extends React.Component {
+  render() {
+    return (
+      <div className='AppName'>
+        <Profile/>
+      </div>
+    );
+  }
+}
+export default App;
