cd915db136f9f17fae71fa197bb34e0854a25022
Oaes Kuruni <oaeskuruni27@gmail.com>
2020-09-26 16:10:46 +0600

    profile card done

--- src/components/profile/index.jsx
+++ src/components/profile/index.jsx
@@ -1,13 +1,42 @@
-import React from 'react'
+import React from "react";
+import "./profile.style.css";
 
-class Profile extends React.Component{
-    render(){
-        return(
-            <div>
-                <h2>i am profile</h2>
-            </div>
-        )
-    }
+class Profile extends React.Component {
+  render() {
+    return (
+      <div className="container">
+        <div className="bio">
+          <h3 className='bioText'>Oaes kuruni</h3>
+          <p>Front-End Software Engineer</p>
+        </div>
+        <div className="skills">
+          <h3 className='skill'>Skills:</h3>
+          <ul>
+            <li>JavaScript</li>
+            <li>React Js</li>
+            <li>Node Js</li>
+          </ul>
+        </div>
+        <div className="socialLinks">
+          <h3 className='links'>Socials Link:</h3>
+          <ul>
+            <li>
+              <a href="facebook.com">Facebook</a>
+            </li>
+            <li>
+              <a href="linkedin.com">LinkedIn</a>
+            </li>
+            <li>
+              <a href="youtube.com">YouTube</a>
+            </li>
+            <li>
+              <a href="youtube.com">GitHub</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default Profile;
