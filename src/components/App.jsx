e193d2ae9c1e78ff24729013de89017beb07df78
Oaes Kuruni <oaeskuruni27@gmail.com>
2020-09-28 11:20:10 +0600

    practicing timmer apps

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,17 +1,33 @@
 import React from "react";
-import Profile from "./profile";
-import Bio from "./profile/Bio";
-import Skill from "./profile/Skill";
-import Test from "./propsTest/Test";
 
 class App extends React.Component {
+  state = {
+    count: 0,
+  };
+
+  incrementCount = () => {
+    this.setState({ count: this.state.count + 1 });
+  };
+
+  decrementCount = () => {
+    if (this.state.count > 0) {
+      this.setState({ count: this.state.count - 1 });
+    }
+  };
+
   render() {
     return (
       <div className="AppName">
-        <Profile />
-        <Test name="Turjo" age="25" />
-        <Bio name='Anis' title='Student'/>
-        <Skill skillA='VueJs' skillB='Angular' skillC='Micro'/>
+        <h4>Simple Timer</h4>
+        <div className="container">
+          <button className="btn" onClick={this.decrementCount}>
+            -
+          </button>
+          <span>{this.state.count}</span>
+          <button className="btn" onClick={this.incrementCount}>
+            +
+          </button>
+        </div>
       </div>
     );
   }
