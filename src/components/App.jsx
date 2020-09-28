34c2f95dc7844e60979200f281800c7dd2cffd61
Oaes Kuruni <oaeskuruni27@gmail.com>
2020-09-28 18:47:34 +0600

    event handling practice

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,73 +1,31 @@
 import React from "react";
-
 class App extends React.Component {
-  state = {
-    count: 0,
+  handlingEvent = (e) => {
+    console.log('kiss me');
   };
 
-  intervalId = null;
-
-  incrementCount = () => {
-    this.setState({ count: this.state.count + 1 });
-  };
-
-  decrementCount = () => {
-    if (this.state.count > 0) {
-      this.setState({ count: this.state.count - 1 });
-    }
-  };
-
-  startTimer = () => {
-    if (this.state.count > 0 && !this.intervalId) {
-      this.intervalId = setInterval(() => {
-        this.setState({ count: this.state.count - 1 }, () => {
-          if (this.state.count === 0) {
-            alert("timer finished");
-            clearInterval(this.intervalId);
-            this.intervalId = null;
-          }
-        });
-      }, 1000);
-    }
-  };
-
-  stopTimer = () => {
-    if (this.intervalId) {
-      clearInterval(this.intervalId);
-      this.intervalId = null;
-    }
-  };
-
-  resetTimer = () => {
-    this.setState({ count: 0 });
-    clearInterval(this.intervalId);
-    this.intervalId = null;
+  handleTextField = (e) => {
+    console.log(e.target.value);
   };
 
   render() {
     return (
       <div className="AppName">
-        <h4>Simple Timer</h4>
-        <div className="container">
-          <button className="btn" onClick={this.decrementCount}>
-            -
-          </button>
-          <span>{this.state.count}</span>
-          <button className="btn" onClick={this.incrementCount}>
-            +
-          </button>
-        </div>
-        <div className="container">
-          <button onClick={this.startTimer} className="btn">
-            Start
-          </button>
-          <button onClick={this.stopTimer} className="btn">
-            Stop
-          </button>
-          <button onClick={this.resetTimer} className="btn">
-            Reset
-          </button>
-        </div>
+        <h4>Event Handling</h4>
+        <button
+          onClick={this.handlingEvent}
+          type="button"
+          className="btn btn-primary"
+        >
+          Love me
+        </button>
+        <br />
+        <input
+          onChange={this.handleTextField}
+          type="text"
+          placeholder="write something"
+          className="textField"
+        />
       </div>
     );
   }
