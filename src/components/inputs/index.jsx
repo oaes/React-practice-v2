c029bb225ebc97c10a0d8d8b75fc3478c9df6e99
Oaes Kuruni <oaeskuruni27@gmail.com>
2020-10-02 11:59:28 +0600

    basic form is done

--- src/components/inputs/index.jsx
+++ src/components/inputs/index.jsx
@@ -7,22 +7,51 @@ class Inputs extends React.Component {
     bio: "",
     birthday: "",
   };
+  handleEvent = (event) => {
+    this.setState({
+      [event.target.name]: event.target.value,
+    });
+  };
   render() {
+    const { name, country, bio, birthday } = this.state;
     return (
       <div>
         <input
           type="text"
+          name="name"
+          value={name}
           placeholder="write your name"
           className="form-control my-2"
+          onChange={this.handleEvent}
         />
-        <select name="country" className="form-control my-2">
+        <select
+          name="country"
+          value={country}
+          className="form-control my-2"
+          onChange={this.handleEvent}
+        >
+          <option>select your country</option>
           <option value="Bangladesh">Bangladesh</option>
           <option value="India">India</option>
           <option value="Nepal">Nepal</option>
           <option value="Japan">Japan</option>
         </select>
-        <textarea name="textarea" className="form-control my-2"></textarea>
-        <input type="date" name="birthday" className='form-control'/>
+        <textarea
+          name="bio"
+          value={bio}
+          className="form-control my-2"
+          onChange={this.handleEvent}
+        ></textarea>
+        <input
+          type="date"
+          name="birthday"
+          value={birthday}
+          className="form-control"
+          onChange={this.handleEvent}
+        />
+        <button className='my-2' onClick={()=> console.log(this.state)}>
+            sent data
+        </button>
       </div>
     );
   }
