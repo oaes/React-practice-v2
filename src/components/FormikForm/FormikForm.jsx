ed0ada79b4439c5fb657a4f4ccf81bd769c9bd08
Oaes Kuruni <oaeskuruni27@gmail.com>
2020-10-16 13:17:27 +0600

    form validation start

--- src/components/FormikForm/FormikForm.jsx
+++ src/components/FormikForm/FormikForm.jsx
@@ -4,15 +4,32 @@ import { useFormik } from "formik";
 const FormikForm = () => {
   const formik = useFormik({
     initialValues: {
-      name: "oaes kuruni",
+      name: "",
       email: "",
       channel: "",
     },
     onSubmit: (values) => {
       console.log(values);
     },
+    validate: (values) => {
+      let errors = {};
+      if (!values.name) {
+        errors.name = require;
+      }
+      if (!values.email) {
+        errors.email = require;
+      } else if (
+        !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)
+      ) {
+        errors.email = "Invalid email address";
+      }
+      if (!values.channel) {
+        errors.channel = require;
+      }
+      return errors;
+    },
   });
-  console.log(formik.values);
+
   return (
     <div>
       <form onSubmit={formik.handleSubmit}>
